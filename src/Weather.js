e39fa08e93db712c829a698135c9ab6019a84702
Roos de Jonge <rosemariedejonge@gmail.com>
2022-12-02 16:14:04 +0100

    Added Axios + weather API and the date+time

--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import FormattedDate from "./FormattedDate.js";
 import "./Weather.css";
 import image from "./images/clear-sky-day.png";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Type a city.."
-              autoFocus="on"
-            />
+  let [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleData(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      icon: response.data.condition.icon,
+      date: new Date(response.data.time * 1000),
+      city: response.data.city,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Type a city.."
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-        </div>
-        <h1 className="city">New York</h1>
-        <ul className="time">
-          <li>Last updated: Wednesday 10:00</li>
-        </ul>
-        <div className="row">
-          <div className="col-sm-6 prospects">
-            <img src={image} alt="sun" />
-            <div className="temp">19</div>
-            <span className="units">
-              <a href="https://www.shecodes.io/">°C</a> |
-              <a href="https://www.shecodes.io/">°F</a>
-            </span>
-          </div>
-          <div className="col-6 mt-3">
-            <ul>
-              <li>Partly Cloudy</li>
-              <li>Humidity: 80%</li>
-              <li>Wind: 10km/h</li>
-            </ul>
+          <h1 className="city">{weatherData.city}</h1>
+          <ul className="time">
+            <li>
+              Last updated:
+              <FormattedDate date={weatherData.date} />
+            </li>
+          </ul>
+          <div className="row">
+            <div className="col-sm-6 prospects">
+              <img src={image} alt="sun" />
+              <div className="temp">{Math.round(weatherData.temperature)}</div>
+              <span className="units">
+                <a href="https://www.shecodes.io/">°C</a> |
+                <a href="https://www.shecodes.io/"> °F</a>
+              </span>
+            </div>
+            <div className="col-6 mt-sm-3">
+              <ul>
+                <li className="text-capitalize">{weatherData.description}</li>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {Math.round(weatherData.wind)}km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "68e4f0522f46113a622241oetb1b9619";
+    let city = "Amsterdam";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleData);
+
+    return <h1>loading..</h1>;
+  }
 }
